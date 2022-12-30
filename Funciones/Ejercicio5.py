deb3ddc9eb1a3b0630cc58b210c3ef3df49268d0
Andres Castillo <Andressempai03@gmail.com>
2022-12-29 23:11:17 -0500

    Funcion para saber si es un año biciesto

---
+++ Funciones/Ejercicio5.py
@@ -0,0 +1,15 @@
+def DefinirBisciesto(año):
+    
+    if año % 4 != 0: 
+	    print("No es bisiesto")
+
+    elif año % 4 == 0 and año % 100 != 0:
+	      print("Es bisiesto")
+
+    elif año % 4 == 0 and año % 100 == 0 and año % 400 != 0: 
+	      print("No es bisiesto")
+
+    elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0: 
+	      print("Es bisiesto")
+
+DefinirBisciesto(2020)
