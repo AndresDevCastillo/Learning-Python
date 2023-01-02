3a90ebc0dad58f73cae4016b59b4cb02f97a4a1a
Andres Castillo <Andressempai03@gmail.com>
2023-01-02 17:27:59 -0500

    llamar funcion desde modulo

---
+++ Modulos/ejercicio 1/Operaciones.py
@@ -0,0 +1,15 @@
+def suma(a, b):
+    print("Suma")
+    return (print(a+b)) 
+
+def resta(a, b):
+    print("Resta")
+    return (print(a-b))
+
+def multiplicacion(a, b):
+    print("Multicacion")
+    return (print(a*b))
+
+def division(a, b):
+    print("Division")
+    return (print(a-b))
---
+++ Modulos/ejercicio 1/main.py
@@ -0,0 +1,6 @@
+import Operaciones as o
+
+o.suma(2, 2)
+o.resta(4, 2)
+o.multiplicacion(2, 2)
+o.division(2, 1)
