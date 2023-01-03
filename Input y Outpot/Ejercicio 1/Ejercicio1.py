0b75b7b5a032f9c242c13f598f068d3fe218afb4
Andres Castillo <Andressempai03@gmail.com>
2023-01-02 23:38:28 -0500

    leer y escribir fichero

---
+++ Input y Outpot/Ejercicio 1/Ejercicio1.py
@@ -0,0 +1,10 @@
+fichero = open('input y Outpot/Ejercicio 1/MiFichero.txt','w')
+
+"Aqui esta el texto" 
+fichero.write("Aqui esta el texto\n" )
+fichero.write("Aqui esta el segundo texto\n" )
+
+print(fichero.readline)
+
+fichero.close()
+
